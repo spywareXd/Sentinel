3fc52bccbea9c1fc5fe5211e4b2fc1deb3947b8e
Ashwin <rajashwin1909@gmail.com>
2026-04-04 16:36:05 +0530

    deployment bug fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/Sentinel',
-  assetPrefix: '/Sentinel/',
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
